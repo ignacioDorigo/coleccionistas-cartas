4167e36149ba35a3c5f08fdf4250c98abf610eb5
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2024-10-03 00:20:57 -0300

    falta buscar los favoritos desde el front, ya trae id de cards del back

--- coleccionistas-front/src/screens/Coleccion/MisSetsPokemon/MisSetsPokemon.js
+++ coleccionistas-front/src/screens/Coleccion/MisSetsPokemon/MisSetsPokemon.js
@@ -1,33 +1,37 @@
-import React, { useEffect, useState, useContext } from 'react'
-import { View, Text, Touchable, TouchableOpacity } from 'react-native'
-import axios from 'axios'
+import React, { useEffect, useState, useContext } from "react";
+import { View, Text, Touchable, TouchableOpacity } from "react-native";
+import axios from "axios";
 
 // Contexto
-import { AuthContext } from '../../../context/AuthContext'
+import { AuthContext } from "../../../context/AuthContext";
 
 export function MisSetsPokemon({ navigation }) {
-    const { isLoggedIn } = useContext(AuthContext);
-    const mail = isLoggedIn;
+  const { isLoggedIn } = useContext(AuthContext);
+  const mail = isLoggedIn;
 
-    const [misSets, setMisSets] = useState([]);
+  const [misSets, setMisSets] = useState([]);
 
-    useEffect(() => {
-        axios.get(`http:/192.168.1.5:8080/coleccionistas/misSets?mail=${mail}`)
-            .then(response => setMisSets(response.data))
-            .catch(error => console.log(error))
-    }, [])
-    return (
-        <View>
-            <Text>Mis Sets Pokemon</Text>
-
-            {misSets.map((set, index) => (
-                <View key={index}>
-                    <TouchableOpacity onPress={() => { navigation.navigate("MisCartasSet", { set: set }) }}>
-                        <Text>{set.id_set}</Text>
-                    </TouchableOpacity>
-                </View>
-            ))}
-
+  useEffect(() => {
+    axios
+      .get(`http:/192.168.1.5:8080/coleccionistas/misSets?mail=${mail}`)
+      .then((response) => {
+        setMisSets(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <View>
+      {misSets.map((set, index) => (
+        <View key={index}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("MisCartasSet", { set: set });
+            }}
+          >
+            <Text>{set.id_set}</Text>
+          </TouchableOpacity>
         </View>
-    )
+      ))}
+    </View>
+  );
 }
--- coleccionistas-front/src/screens/Favoritos/FavoritosScreen/FavoritosScreen.js
+++ coleccionistas-front/src/screens/Favoritos/FavoritosScreen/FavoritosScreen.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import styles from './FavoritosScreen.styles'
+import React, { useContext, useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import styles from "./FavoritosScreen.styles";
+import { AuthContext } from "../../../context/AuthContext";
+import axios from "axios";
 
 export function FavoritosScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Wishlist</Text>
-        </View>
-    );
-};
+  const { isLoggedIn } = useContext(AuthContext);
+  const mail = isLoggedIn;
+  const [cardsCompletas, setCardsCompletas] = useState([]);
+  const [favoritos, setFavoritos] = useState([]);
+
+  const buscarCardsCompletas = (favoritos) => {
+    const cards = [];
+    for (let index = 0; index < favoritos.length; index++) {
+      const favorito = favoritos[index];
+      axios
+        .get(`https://api.pokemontcg.io/v2/cards?q=id:${favorito.id}`)
+        .then((response) => {
+          cards.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      console.log(favorito);
+    }
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:8080/coleccionistas/misFavoritosPokemon?mail=${mail}`
+      )
+      .then((response) => {
+        setFavoritos(response.data);
+      }).then(datos)
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {favoritos.map((favorito, index) => (
+        <Text key={index}>ID DE LA CARD FAVORITA: {favorito.id_card}</Text>
+      ))}
+    </View>
+  );
+}
