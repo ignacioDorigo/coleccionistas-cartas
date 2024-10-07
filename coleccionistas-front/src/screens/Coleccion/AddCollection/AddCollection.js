319838fe1e3628f1df28fb36a5e555e98869e3e1
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2024-10-07 17:45:54 -0300

    un poco de mejora de la estetica

--- coleccionistas-front/src/screens/Coleccion/AddCollection/AddCollection.js
+++ coleccionistas-front/src/screens/Coleccion/AddCollection/AddCollection.js
@@ -1,50 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
+import axios from "axios";
 
+import { styles } from "./AddCollection.styles";
 // Fichero Screen
-import { screen } from '../../../utils'
+import { screen } from "../../../utils";
 
 export function AddCollection({ navigation }) {
+  const [colecciones, setColecciones] = useState([]);
 
-    const [colecciones, setColecciones] = useState([]);
-
-    const ViajarColeccion = (coleccion) => {
-        if (coleccion.nombre === 'Pokemon') {
-            navigation.navigate(screen.coleccion.elegirSetPokemon, { coleccion: coleccion });
-        }
+  const ViajarColeccion = (coleccion) => {
+    if (coleccion.nombre === "Pokemon") {
+      navigation.navigate(screen.coleccion.elegirSetPokemon, {
+        coleccion: coleccion,
+      });
     }
+    if (coleccion.nombre === "Yugioh") {
+        Alert.alert("Aviso","La colección de YuGiOh estara disponible en el proximo sprint");
+    }
+  };
 
-    useEffect(() => {
-        axios.get(`http://192.168.1.14:8080/coleccionistas/coleccionesDisponibles`)
-            .then((respuestaBack) => { setColecciones(respuestaBack.data) })
-            .catch((error) => { console.log(error) })
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`http://192.168.1.14:8080/coleccionistas/coleccionesDisponibles`)
+      .then((respuestaBack) => {
+        setColecciones(respuestaBack.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    return (
-        <ScrollView style={style.container}>
-            <Text>Elegí Tu Nueva Coleccion</Text>
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.viewHeader}>
+        <Text style={styles.header}>Colecciones Disponibles</Text>
+        <Text style={styles.subtitle}>Elegí el tema sobre el cual queres empezar a coleccionar</Text>
+      </View>
 
-            {colecciones.map((coleccion, index) => (
-                <TouchableOpacity key={index} onPress={() => ViajarColeccion(coleccion)}>
-                    <Image style={style.imagen} source={{ uri: `${coleccion.imagen}` }}></Image>
-                </TouchableOpacity>
-            ))
-            }
-
-        </ScrollView >
-    )
+      {colecciones.map((coleccion, index) => (
+        <TouchableOpacity
+          key={index}
+          onPress={() => ViajarColeccion(coleccion)}
+          style={styles.touchable}
+        >
+          <Image
+            style={styles.image}
+            source={{ uri: `${coleccion.imagen}` }}
+          ></Image>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  );
 }
-
-const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-    },
-    imagen: {
-        width: '100%',
-        height: 120,
-        marginBottom: 10,
-    }
-    ,
-})
