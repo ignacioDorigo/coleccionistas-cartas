097378e4ff1b1bcf11c231ea71f3333143d14b31
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2024-10-22 13:22:01 -0300

    UNIFICACION QUERYS SQL

--- QuerysMySQL.sql
+++ QuerysMySQL.sql
@@ -1,3 +1,16 @@
+CREATE DATABASE coleccionistas;
+
+USE coleccionistas;
+
+CREATE TABLE usuarios(
+	mail VARCHAR(50) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	edad INT NOT NULL,
+	nombre VARCHAR(30) NOT NULL,
+	apellido VARCHAR(30) NOT NULL,
+	PRIMARY KEY (mail)
+);
+
 CREATE TABLE colecciones(
 	id INTEGER NOT NULL,
 	nombre VARCHAR(30) NOT NULL,
@@ -37,7 +50,6 @@ CREATE TABLE usuarios_cards(
 	FOREIGN KEY (mail) REFERENCES usuarios(mail)
 );
 
-/*Nueva tabla para favoritos pokemon*/
 CREATE TABLE favoritos_pokemon( 
 	id INTEGER AUTO_INCREMENT NOT NULL,
     mail VARCHAR(50) NOT NULL,
