7748e776a785aca61d34bb3bc6cc7ee680b33be0
Mafetita <mafebohorquezh@gmail.com>
2024-11-20 00:09:23 -0300

    Fix chatgpt call

--- coleccionistas-back/src/main/java/com/example/demo/controller/Controlador.java
+++ coleccionistas-back/src/main/java/com/example/demo/controller/Controlador.java
@@ -1,13 +1,11 @@
 package com.example.demo.controller;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.modelo.Avatar;
